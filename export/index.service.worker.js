a3c2c6786bbd33ee6803b4738952b33e981fd1d6
baltazarparra <baltazarparra@outlook.com>
2026-06-02 20:30:27 -0300

    feat: beta 8

--- export/index.service.worker.js
+++ export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780441919|3127924';
+const CACHE_VERSION = '1780443015|2900126';
 /** @type {string} */
 const CACHE_PREFIX = 'caipora-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
